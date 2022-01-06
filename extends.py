30f57e704a6d2597cf71852c0a66926951d2686b
carolis <65249499+carolina-ml@users.noreply.github.com>
2022-01-05 21:49:41 -0300

    heranca class

---
+++ extends.py
@@ -0,0 +1,14 @@
+class Animal:
+    alma = True
+    peso = 0
+    nome = ''
+    vida = True
+
+class Sapo(Animal):
+    patas = 4
+
+class Cobra(Animal):
+    escamas = 50
+    
+
+print(Cobra.peso)
